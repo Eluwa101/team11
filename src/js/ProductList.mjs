023545639ac06bf2387d8356f7e0325ba050cba5
Bernspecial <michaelbernardolayemi@gmail.com>
2024-10-03 15:57:00 +0100

    individual activity

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -4,7 +4,7 @@ import { renderListWithTemplate, getParams } from "./utils.mjs";
 function productCardTemplate(product) {
     return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.name}">
+      <img src="${product.Image.PrimaryMedium}" alt="Image of ${product.name}">
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.Name}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p>
@@ -25,7 +25,7 @@ export default class ProductList {
     }
 
     async init() {
-        const products = await this.dataSource.getData();
+        const products = await this.dataSource.getData(this.category);
         const filteredProducts = this.filterProducts(products);
         this.products = filteredProducts;
         this.renderProductList(filteredProducts);
